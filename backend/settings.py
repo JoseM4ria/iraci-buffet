1cef0331019c44d2081737285c3287273d10ce5a
ZeMaria <duartemaf@gmail.com>
2024-09-16 10:50:51 -0300

    segundo commit-correcoes

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend.core',
+    'backend.core.customers',
+    'backend.products',
+    'backend.sales',
+
 ]
 
 MIDDLEWARE = [
